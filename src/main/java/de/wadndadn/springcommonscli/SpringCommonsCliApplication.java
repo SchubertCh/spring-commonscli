f0e40f7200daf09297f2de3cae2597bd3277c711
Christian Schubert <544196+SchubertCh@users.noreply.github.com>
2023-01-17 11:25:37 +0100

    Added files from project created with IntelliJ IDEA

---
+++ src/main/java/de/wadndadn/springcommonscli/SpringCommonsCliApplication.java
@@ -0,0 +1,13 @@
+package de.wadndadn.springcommonscli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommonsCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCommonsCliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/wadndadn/springcommonscli/SpringCommonsCliApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wadndadn.springcommonscli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommonsCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
